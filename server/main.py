557eb15767c896666d50571460cc094a2c7f6b8c
John Walling <j00walling@gmail.com>
2024-03-05 00:02:27 -0800

    retrieve s3 model in main

--- server/main.py
+++ server/main.py
@@ -1,14 +1,47 @@
 import tensorflow as tf
 import numpy as np
 import shutil
+import boto3
+import os
+import tempfile
 
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from tempfile import NamedTemporaryFile
 from keras.preprocessing import image
+from keras.models import load_model, model_from_json
 
 
 app = FastAPI()
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+BUCKET_NAME = os.environ.get('BUCKET_NAME')
+MODEL_KEY = os.environ.get('MODEL_KEY')
+
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+)
+
+def load_model_from_s3(bucket_name, model_key, data_to_predict):
+    # Initialize the S3 client
+    s3 = boto3.client('s3')
+
+    # Create a temporary file
+    with tempfile.NamedTemporaryFile(suffix=".h5") as temp_file:
+        # Download the file from S3 to the temporary file
+        s3.download_file(bucket_name, model_key, temp_file.name)
+
+        model = load_model(temp_file.name)
+
+    # Make predictions
+    predictions = model.predict(data_to_predict)
+
+    return predictions
+    
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -30,13 +63,13 @@ async def predict_age(file: UploadFile = File(...)):
         with NamedTemporaryFile(delete=False) as temp_file:
             shutil.copyfileobj(file.file, temp_file)
 
-            model = tf.saved_model.load("server/cnn/model_versions/v1")
             img = image.load_img(temp_file.name, target_size=(180, 180))
             img_ary = image.img_to_array(img)
             img_ary = np.expand_dims(img_ary, axis=0)
             img_ary /= 255.0
 
-            predicted_age = int(model(img_ary).numpy()[0][0])
+            response = load_model_from_s3(BUCKET_NAME, MODEL_KEY, img_ary)
+            predicted_age = int(response[0][0])
             return {"predicted_age": predicted_age}
     
     except Exception as e:
