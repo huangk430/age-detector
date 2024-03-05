27b7fa3a1e757a488793e7d042cfc7d587e74ee4
Kelly Huang <huangk430@gmail.com>
2024-03-05 15:51:07 -0800

    load model outside of endpoint to save memory

--- server/main.py
+++ server/main.py
@@ -5,11 +5,13 @@ import boto3
 import os
 import tempfile
 
+
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from tempfile import NamedTemporaryFile
 from keras.preprocessing import image
-from keras.models import load_model, model_from_json
+from keras.models import load_model
+
 
 
 app = FastAPI()
@@ -19,28 +21,19 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 BUCKET_NAME = os.environ.get('BUCKET_NAME')
 MODEL_KEY = os.environ.get('MODEL_KEY')
 
+
 s3 = boto3.client(
     's3',
     aws_access_key_id=AWS_ACCESS_KEY_ID,
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY
 )
 
-def load_model_from_s3(bucket_name, model_key, data_to_predict):
-    # Initialize the S3 client
-    s3 = boto3.client('s3')
+#Load the model from s3
+with tempfile.NamedTemporaryFile(suffix=".h5") as temp_file:
+    # Download the file from S3 to the temporary file
+    s3.download_file(BUCKET_NAME, MODEL_KEY, temp_file.name)
 
-    # Create a temporary file
-    with tempfile.NamedTemporaryFile(suffix=".h5") as temp_file:
-        # Download the file from S3 to the temporary file
-        s3.download_file(bucket_name, model_key, temp_file.name)
-
-        model = load_model(temp_file.name)
-
-    # Make predictions
-    predictions = model.predict(data_to_predict)
-
-    return predictions
-    
+    model = load_model(temp_file.name)
 
 
 app.add_middleware(
@@ -68,7 +61,7 @@ async def predict_age(file: UploadFile = File(...)):
             img_ary = np.expand_dims(img_ary, axis=0)
             img_ary /= 255.0
 
-            response = load_model_from_s3(BUCKET_NAME, MODEL_KEY, img_ary)
+            response = model.predict(img_ary)
             predicted_age = int(response[0][0])
             return {"predicted_age": predicted_age}
     
